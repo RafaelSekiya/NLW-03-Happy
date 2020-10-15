6878f07829d2c41514cfc0daf29c45275abeaac9
Rafael Sekiya <rafaelsekiya7070@outlook.com>
2020-10-15 10:33:09 -0300

    added package-lock.json, package.json, src, views, database

---
+++ src/database/fakedata.js
@@ -0,0 +1,36 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-23.4839906",
+        lng: "-47.4891743",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 05 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1594007184512-2a607d1df588?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1597992350431-56cb7e28a7a2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+
+
+
+    },
+    {
+        id: 2,
+        lat: "-23.4839906",
+        lng: "-47.4691743",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 05 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1594007184512-2a607d1df588?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1597992350431-56cb7e28a7a2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+
+    }
+]
